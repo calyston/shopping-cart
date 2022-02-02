559b25ec9dcc1fa96a7ac4f2f631f35d7e9c55c4
Christine <calyston.14@gmail.com>
2022-02-02 10:45:26 +0000

    style: cart container styled

--- src/CartItem.js
+++ src/CartItem.js
@@ -5,13 +5,15 @@ import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
 const CartItem = ({ id, title, price, img, quantity }) => {
   return (
     <article className="cart-item">
-      <img src={img} alt={title} />
-      <div>
-        <h4>{title}</h4>
-        <p className="item-price">£{price}</p>
-        <button className="remove-item" onClick={() => console.log('remove item')}>Remove</button>
+      <div className="item-details">
+        <img src={img} alt={title} />
+        <div>
+          <h4>{title}</h4>
+          <p className="item-price">£{price}</p>
+          <button className="remove-button" onClick={() => console.log('remove item')}>Remove</button>
+        </div>
       </div>
-      <div>
+      <div className="quantity-box">
         <button className="quantity-button" onClick={() => console.log('increase quantity')}><FaChevronUp /></button>
         <p className="quantity">{quantity}</p>
         <button className="quantity-button" onClick={() => console.log('decrease quantity')}><FaChevronDown /></button>
